e16c13a04e8c5fbcf538fbf370b17e84d8c65739
Laurentiu Lucaci <ext_laurentiu.lucaci@btrl.ro>
2019-08-14 13:33:54 +0300

    Emag LogIn 2

--- src/test/java/Testare/features/search/EmagLogInTest.java
+++ src/test/java/Testare/features/search/EmagLogInTest.java
@@ -6,9 +6,12 @@ import net.thucydides.core.annotations.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import Testare.steps.serenity.EndUserSteps;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 @RunWith(SerenityRunner.class)
 public class EmagLogInTest {
@@ -19,10 +22,14 @@ public class EmagLogInTest {
     @Steps
     public EmagLogInSteps laurentiu;
 
+
     @Test
     public void emagLogIn()
     {
         laurentiu.is_the_home_page();
         laurentiu.loginEmag("testarelaurentiu@gmail.com","AstaEParola");
     }
+
+
+
 }
