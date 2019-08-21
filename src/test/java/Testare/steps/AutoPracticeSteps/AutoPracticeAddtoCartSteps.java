e248515e81f94d8b36cd7f3fa74488dca0a2dec0
Madalina Vaida <ext_madalina.vaida@btrl.ro>
2019-08-21 15:37:43 +0300

    CSV Test Logic For Auto

--- src/test/java/Testare/steps/AutoPracticeSteps/AutoPracticeAddtoCartSteps.java
+++ src/test/java/Testare/steps/AutoPracticeSteps/AutoPracticeAddtoCartSteps.java
@@ -9,6 +9,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
 
@@ -20,7 +21,7 @@ public class AutoPracticeAddtoCartSteps {
     AutoPracticeShoppingPage autoShoppingPage;
 
     @Step
-    public void selectFirstItem(){
+    public void selectFirstItem() {
         autoAddPage.selectFirstItem();
     }
 
@@ -34,7 +35,7 @@ public class AutoPracticeAddtoCartSteps {
     }
 
     @Step
-    public void openCart(){
+    public void openCart() {
         autoAddPage.openCart();
     }
 
@@ -46,20 +47,35 @@ public class AutoPracticeAddtoCartSteps {
     @Step
     public void getTitleFromItem(WebElementFacade item) {
         String title = item.findElement(By.xpath("//*[@id=\"product_7_34_0_203911\"]/td[2]/p/a")).getText();
-        Serenity.getCurrentSession().put("returnTitle",title);
+        Serenity.getCurrentSession().put("returnTitle", title);
     }
 
     @Step
-    public void assertSearch(){
+    public void assertSearch() {
         String actual = autoShoppingPage.returnTitle().getText().toLowerCase();
         String expected = (String) Serenity.getCurrentSession().get("returnTitle");
         Assert.assertEquals("The item was not added", actual, expected.toLowerCase());
     }
 
     @StepGroup
-    public void groupOfSteps(){
+    public void groupOfSteps() {
         getTitleFromItem(autoShoppingPage.returnTitle());
         assertSearch();
+    }
+
+
+    @Step
+    public void assertDescription(String expected) {
+        String actual = autoAddPage.returnDescription().getText().toLowerCase();
+        Assert.assertEquals("The item was not added", actual, expected.toLowerCase());
+    }
+
+
+
+    @Step
+    public void assertPrice(String expected) {
+        String actual = autoAddPage.returnPrice().getText().toLowerCase();
+        Assert.assertThat (actual, containsString(expected));
     }
 
 }
